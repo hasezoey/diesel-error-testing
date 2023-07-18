88bffb3b20949cc1e7a98181093e401afbfabe5c
hasezoey <hasezoey@gmail.com>
2023-07-18 15:29:29 +0200

    fixed

--- src.rs
+++ src.rs
@@ -14,11 +14,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let mut conn =  SqliteConnection::establish("/tmp/test.db")?;
 
 	let input = 10;
-	let subquery = FilterDsl::filter(tableA.select(columnA), rowid.eq(input)).single_value();
+	let tablea_alias = diesel::alias!(crate::tableA as tablea_alias);
+	let subquery = FilterDsl::filter(tablea_alias.select(tablea_alias.field(columnA)), tablea_alias.field(rowid).eq(input)).single_value();
 
 	let res = FilterDsl::filter(tableA, columnA.nullable().eq(subquery))
 		.count()
-		.get_result(&mut conn)?;
+		.get_result::<i64>(&mut conn)?;
 
 	println!("res {:#?}", res);
 
