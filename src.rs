d0a4b89d51a430eee5a4c054d0a6f1d61bfdc8b1
hasezoey <hasezoey@gmail.com>
2023-07-24 13:52:56 +0200

    rust-analyzer issue

--- src.rs
+++ src.rs
@@ -1,27 +1,17 @@
-use diesel::query_dsl::methods::FilterDsl;
-
-diesel::table! {
-	tableA (rowid) {
-		rowid -> Integer,
-		columnA -> Integer,
+mod testy {
+	pub struct Testy {
+		pub field: String,
 	}
 }
 
+/// Should resolve to field [testy::Testy::field] `test`
+fn hello() {
+	todo!();
+}
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	use diesel::prelude::*;
-	use crate::tableA::dsl::*;
-
-	let mut conn =  SqliteConnection::establish("/tmp/test.db")?;
-
-	let input = 10;
-	let tablea_alias = diesel::alias!(crate::tableA as tablea_alias);
-	let subquery = FilterDsl::filter(tablea_alias.select(tablea_alias.field(columnA)), tablea_alias.field(rowid).eq(input)).single_value();
-
-	let res = FilterDsl::filter(tableA, columnA.nullable().eq(subquery))
-		.count()
-		.get_result::<i64>(&mut conn)?;
-
-	println!("res {:#?}", res);
+	hello();
 
 	return Ok(());
 }
