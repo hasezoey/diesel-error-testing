0b902a520cda3d7de72c943beee21070b1967cac
hasezoey <hasezoey@gmail.com>
2023-08-21 19:20:14 +0200

    rust-analyzer macro no completion

--- src.rs
+++ src.rs
@@ -1,17 +1,20 @@
-mod testy {
-	pub struct Testy {
-		pub field: String,
-	}
-}
-
-/// Should resolve to field [testy::Testy::field] `test`
-fn hello() {
-	todo!();
-}
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	hello();
+	use crossbeam::channel::*;
+
+	let (tx1, rx1) = unbounded::<usize>();
+	let (tx2, rx2) = unbounded::<usize>();
+
+	// if the following is just *one* "recv", completion works until something invalid
+	select! {
+		recv(rx1) -> msg => {
+			// no completion at all
+		},
+		recv(rx2) -> msg => {
+			// no completion at all
+		},
+		default => (),
+	}
 
 	return Ok(());
 }
